eff35ff7465eba61d8948c63654483d2be190ef8
George Holt <george.holt@stfc.ac.uk>
2023-07-24 15:07:45 +0100

    Documenting circuit_eq_metal.py

--- freegsnke/circuit_eq_metal.py
+++ freegsnke/circuit_eq_metal.py
@@ -7,9 +7,28 @@ from .implicit_euler import implicit_euler_solver
 
 
 class metal_currents:
-    # sets up framework for all metal currents
-    # calculates residuals of full non linear circuit eq 
-    # sets up to solve metal circuit eq for vacuum shots
+    """Sets up framework for all metal currents. Calculates residuals of full
+    nonlinear circuit equation. Sets up to solve metal circuit equation for
+    vacuum shots.
+    
+    Parameters
+    ----------
+    
+    flag_vessel_eig : bool
+        Flag to use vessel eigenmodes.
+    flag_plasma : bool
+        Whether to include plasma in circuit equation. If True, plasma_grids
+        must be provided.
+    plasma_grids : freegsnke.plasma_grids
+        Plasma grids object. Defaults to None.
+    max_mode_frequency : float
+        Maximum frequency of vessel eigenmodes to include in circuit equation.
+        Defaults to 1.
+    max_internal_timestep : float
+        Maximum internal timestep for implicit euler solver. Defaults to .0001.
+    full_timestep : float
+        Full timestep for implicit euler solver. Defaults to .0001.
+    """
     
     def __init__(self, flag_vessel_eig,
                        flag_plasma,
@@ -36,27 +55,39 @@ class metal_currents:
             self.max_mode_frequency = 0
             self.initialize_for_no_eig()
 
-
-        # change if plasma by using set_plasma
         if flag_plasma:
             self.plasma_grids = plasma_grids
             self.Mey = plasma_grids.Mey()            
 
-        #dummy voltage vector
+        # Dummy voltage vector
         self.empty_U = np.zeros(self.n_coils)
         
 
-    def make_selected_mode_mask_from_max_freq(self, ):
+    def make_selected_mode_mask_from_max_freq(self):
+        """Creates a mask for the vessel normal modes to include in the circuit
+        equation based on the maximum frequency of the modes.
+        """
         selected_modes_mask = normal_modes.w_passive < self.max_mode_frequency
-        self.selected_modes_mask = np.concatenate((np.ones(self.n_active_coils).astype(bool),
-                                              selected_modes_mask))
+        self.selected_modes_mask = np.concatenate((
+            np.ones(self.n_active_coils).astype(bool),
+            selected_modes_mask
+        ))
         self.n_independent_vars = np.sum(self.selected_modes_mask)
-        print('Input max_mode_frequency corresponds to', 
-               self.n_independent_vars-self.n_active_coils, 'independent vessel normal modes',
-               'in addition to the', self.n_active_coils, 'active coils.')
+        print('Input max_mode_frequency corresponds to ', 
+               self.n_independent_vars-self.n_active_coils,
+               ' independent vessel normal modes in addition to the ',
+               self.n_active_coils, ' active coils.')
         
 
-    def initialize_for_eig(self, selected_modes_mask):     
+    def initialize_for_eig(self, selected_modes_mask):
+        """Initializes the metal currents object for the case where vessel
+        eigenmodes are used.
+        
+        Parameters
+        ----------
+        selected_modes_mask : np.ndarray
+            Mask for the vessel normal modes to include in the circuit equation.
+        """   
         
         self.selected_modes_mask = selected_modes_mask
         self.n_independent_vars = np.sum(self.selected_modes_mask)
@@ -67,13 +98,10 @@ class metal_currents:
         self.Vm1 = ((normal_modes.Vmatrix).T)[selected_modes_mask, :]
         self.V = (normal_modes.Vmatrix)[:, selected_modes_mask]
 
-        # self.Vm1 = ((normal_modes.Vmatrix).T)[:self.n_independent_vars]
-        # self.V = (normal_modes.Vmatrix)[:, :self.n_independent_vars]
-
-        # equation is Lambda**(-1)Iddot + I = F
+        # Equation is Lambda**(-1)Iddot + I = F
         # where Lambda is such that R12@M-1@R12 = V Lambda V-1
         # w are frequences, eigenvalues of Lambda
-        # note Lambda is not diagonal because of active/passive mode separation
+        # Note Lambda is not diagonal because of active/passive mode separation
         self.Lambda = self.Vm1@normal_modes.lm1r@self.V
         self.Lambdam1 = self.Vm1@normal_modes.rm1l@self.V
 
@@ -82,10 +110,12 @@ class metal_currents:
         self.Rm12 = machine_config.coil_resist**-.5
         # R, R12, Rm12 are vectors rather than matrices!
 
-        self.solver = implicit_euler_solver(Mmatrix=self.Lambdam1, 
-                                            Rmatrix=np.eye(self.n_independent_vars), 
-                                            max_internal_timestep=self.max_internal_timestep,
-                                            full_timestep=self.full_timestep)
+        self.solver = implicit_euler_solver(
+            Mmatrix=self.Lambdam1,
+            Rmatrix=np.eye(self.n_independent_vars),
+            max_internal_timestep=self.max_internal_timestep,
+            full_timestep=self.full_timestep
+        )
 
         if self.flag_plasma:
             self.forcing_term = self.forcing_term_eig_plasma
@@ -93,22 +123,24 @@ class metal_currents:
             self.forcing_term = self.forcing_term_eig_no_plasma
 
 
-
-
-    def initialize_for_no_eig(self, ):
+    def initialize_for_no_eig(self):
+        """Initializes the metal currents object for the case where vessel
+        eigenmodes are not used."""
         self.n_independent_vars = self.n_coils
         self.M = machine_config.coil_self_ind
         self.Mm1 = normal_modes.Mm1
         self.R = np.diag(machine_config.coil_resist)
-        self.Rm1 = 1/machine_config.coil_resist #it's a vector!
+        self.Rm1 = 1/machine_config.coil_resist # it's a vector!
         self.Mm1R = self.Mm1@self.R
         self.Rm1M = np.diag(1/machine_config.coil_resist)@self.M
 
-        # equation is MIdot + RI = F
-        self.solver = implicit_euler_solver(Mmatrix=self.M, 
-                                            Rmatrix=self.R, 
-                                            max_internal_timestep=self.max_internal_timestep,
-                                            full_timestep=self.full_timestep)
+        # Equation is MIdot + RI = F
+        self.solver = implicit_euler_solver(
+            Mmatrix=self.M,
+            Rmatrix=self.R,
+            max_internal_timestep=self.max_internal_timestep,
+            full_timestep=self.full_timestep
+        )
 
         if self.flag_plasma:
             self.forcing_term = self.forcing_term_no_eig_plasma
@@ -124,8 +156,25 @@ class metal_currents:
                         max_mode_frequency=1,
                         max_internal_timestep=.0001,
                         full_timestep=.0001):
-        # allows reset of init inputs
+        """Resets init inputs.
         
+        Parameters
+        ----------
+        flag_vessel_eig : bool
+            Flag to use vessel eigenmodes.
+        flag_plasma : bool
+            Whether to include plasma in circuit equation. If True, plasma_grids
+            must be provided.
+        plasma_grids : freegsnke.plasma_grids
+            Plasma grids object. Defaults to None.
+        max_mode_frequency : float
+            Maximum frequency of vessel eigenmodes to include in circuit
+            equation.
+        max_internal_timestep : float
+            Maximum internal timestep for implicit euler solver.
+        full_timestep : float
+            Full timestep for implicit euler solver.
+        """        
         control = (self.max_internal_timestep != max_internal_timestep)
         self.max_internal_timestep = max_internal_timestep
 
@@ -151,89 +200,158 @@ class metal_currents:
             self.initialize_for_no_eig()
 
 
-
-
-
-    # def calc_inverse_operator(self, dt):
-    #     if dt not in self.inverse_operator.keys():
-    #         self.inverse_operator[dt] = np.linalg.inv(np.eye(self.n_independent_vars) + dt*self.Mm1R)
-    #     self.set_inverse_operator = 1.0*self.inverse_operator[dt]
-
-
-    # def set_timesteps(self, full_timestep, max_internal_timestep):
-    #     self.full_timestep = full_timestep
-    #     self.max_internal_timestep = max_internal_timestep
-    #     self.n_steps = int(self.full_timestep/self.max_internal_timestep + .999)
-    #     self.internal_timestep = self.full_timestep/self.n_steps 
-    #     if self.flag_vessel_eig<1:
-    #         self.calc_inverse_operator(self.internal_timestep)
-
-
     def forcing_term_eig_plasma(self, active_voltage_vec, Iydot):
+        """Right-hand-side of circuit equation in eigenmode basis with plasma.
+        
+        Parameters
+        ----------
+        active_voltage_vec : np.ndarray
+            Vector of active coil voltages.
+        Iydot : np.ndarray
+            Vector of rate of change of plasma currents.
+
+        Returns
+        -------
+        all_Us : np.ndarray
+            Voltages.
+        """
         all_Us = np.zeros_like(self.empty_U)
         all_Us[:self.n_active_coils] = active_voltage_vec
         all_Us -= self.Mey@Iydot
         all_Us = np.dot(self.Vm1, self.Rm12*all_Us)
         return all_Us
+    
+
     def forcing_term_eig_no_plasma(self, active_voltage_vec, Iydot=0):
+        """Right-hand-side of circuit equation in eigenmode basis without
+        plasma.
+        
+        Parameters
+        ----------
+        active_voltage_vec : np.ndarray
+            Vector of active coil voltages.
+        Iydot : np.ndarray, optional
+            This is not used.
+            
+        Returns
+        -------
+        all_Us : np.ndarray
+            Voltages."""
         all_Us = self.empty_U.copy()
         all_Us[:self.n_active_coils] = active_voltage_vec
         all_Us = np.dot(self.Vm1, self.Rm12*all_Us)
         return all_Us
+    
+    
     def forcing_term_no_eig_plasma(self, active_voltage_vec, Iydot):
+        """Right-hand-side of circuit equation in normal mode basis with plasma.
+        
+        Parameters
+        ----------
+        active_voltage_vec : np.ndarray
+            Vector of active coil voltages.
+        Iydot : np.ndarray
+            Vector of rate of change of plasma currents.
+
+        Returns
+        -------
+        all_Us : np.ndarray
+            Voltages.
+        """
         all_Us = self.empty_U.copy()
         all_Us[:self.n_active_coils] = active_voltage_vec
         all_Us -= np.dot(self.Mey, Iydot)
         return all_Us
+    
+    
     def forcing_term_no_eig_no_plasma(self, active_voltage_vec, Iydot=0):
+        """Right-hand-side of circuit equation in normal mode basis without
+        plasma.
+
+        Parameters
+        ----------
+        active_voltage_vec : np.ndarray
+            Vector of active coil voltages.
+        Iydot : np.ndarray, optional
+            This is not used.
+
+        Returns
+        -------
+        all_Us : np.ndarray
+            Voltages.
+        """
         all_Us = self.empty_U.copy()
         all_Us[:self.n_active_coils] = active_voltage_vec
         return all_Us
 
 
-
     def IvesseltoId(self, Ivessel):
+        """Given Ivessel, returns Id.
+        
+        Parameters
+        ----------
+        Ivessel : np.ndarray
+            Vessel currents.
+        
+        Returns
+        -------
+        Id : np.ndarray
+
+        """
         Id = np.dot(self.Vm1, self.R12*Ivessel)
         return Id
+    
+    
     def IdtoIvessel(self, Id):
+        """Given Id, returns Ivessel.
+        
+        Parameters
+        ----------"""
         Ivessel = self.Rm12*np.dot(self.V, Id)
         return Ivessel
 
-    
-  
-    # def internal_stepper_eig(self, It, forcing):
-    #     # advances current for equation
-    #     # Lambda^-1 Idot + I = forcing
-    #     # where Lambda is R12@M^-1@R12 = V@Lambda@V^-1
-    #     # I are normal modes: Inormal = V^-1@R12@I
-    #     # forcing here is V^-1@Rm12@(voltage-plasma_inductance), like calculated in forcing_term
-    #     elambdadt = np.exp(-self.internal_timestep*self.Lambda)
-    #     Itpdt = It*elambdadt + (1-elambdadt)*forcing
-    #     return Itpdt
-    # def internal_stepper_no_eig(self, It, forcing):
-    #     # advances current using equation
-    #     # Itpdt = (Mm1@R*dt+1)^-1(forcing+It)
-    #     # forcing is Mm1@(voltage-plasma_inductance),like calculated in forcing_term
-    #     Itpdt = self.set_inverse_operator@(forcing*self.internal_timestep + It)
-    #     return Itpdt
-
 
     def stepper(self, It, active_voltage_vec, Iydot=0):
-        # input It and output I(t+dt) are vessel currents if no_eig or normal modes if eig
+        """Steps the circuit equation forward in time.
 
+        Parameters
+        ----------
+        It : np.ndarray
+            Currents at time t.
+        active_voltage_vec : np.ndarray
+            Vector of active coil voltages.
+        Iydot : np.ndarray or float, optional
+            Vector of rate of change of plasma currents. Defaults to 0.
+
+        Returns
+        -------
+        It : np.ndarray
+            Currents at time t+dt.
+        """
         forcing = self.forcing_term(active_voltage_vec, Iydot)
         It = self.solver(It, forcing)
         return It
 
 
-    # def Iedot(self, It, Itpdt):
-    #     iedot = (Itpdt - It)/self.full_timestep
-    #     return iedot
-
-
     def current_residual(self, Itpdt, Iddot, forcing_term):
-        # returns residual of circuit equations in normal modes:
-        # residual = Lambda^-1 Idot + I - forcing
+        """Calculates the residual of the circuit equation in normal modes.
+        
+        $$\Lambda^{-1} \dot{I} + I - F = \text{residual}$$.
+
+        Parameters
+        ----------
+        Itpdt : np.ndarray
+            Currents at time t+dt.
+        Iddot : np.ndarray
+            Rate of change of currents at time t.
+        forcing_term : np.ndarray
+            Forcing term of circuit equation.
+
+        Returns
+        -------
+        residual : np.ndarray
+            Residual of circuit equation.
+        """
         residual = np.dot(self.Lambdam1, Iddot)
         residual += Itpdt 
         residual -= forcing_term

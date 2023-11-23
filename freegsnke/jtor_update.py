92aec74193c60b547403f8bc9f945758c3b812e2
nicamo <n.amorisco@gmail.com>
2023-11-23 16:22:12 +0000

    improved limiter with bilinear psi interpolation on upsampled limiter

--- freegsnke/jtor_update.py
+++ freegsnke/jtor_update.py
@@ -1,37 +1,8 @@
 import freegs
 import numpy as np
 from freegs import critical
-from . import limiter
+from . import limiter_func
 from . import plasma_grids
-
-from matplotlib.path import Path
-
-
-
-def build_mask_inside_limiter(eq, limiter):
-    """Uses the coordinates of points along the edge of the limiter region
-    to generate the mask of contained domain points.
-
-    Parameters
-    ----------
-    eq : freeGS Equilibrium object
-        Specifies the domain properties
-    limiter : freeGS.machine.Wall object
-        Specifies the limiter contour points
-    Returns
-    -------
-    mask_inside_limiter : np.array
-        Mask over the full domain of grid points inside the limiter region.
-    """
-    verts = np.concatenate((np.array(limiter.R)[:,np.newaxis],
-                            np.array(limiter.Z)[:,np.newaxis]), axis=-1)
-    path = Path(verts)
-
-    points = np.concatenate((eq.R[:,:,np.newaxis], eq.Z[:,:,np.newaxis]), axis=-1)
-    
-    mask_inside_limiter = path.contains_points(points.reshape(-1,2))
-    mask_inside_limiter = mask_inside_limiter.reshape(np.shape(eq.R))
-    return mask_inside_limiter
 
 
 class ConstrainBetapIp(freegs.jtor.ConstrainBetapIp):
@@ -50,9 +21,12 @@ class ConstrainBetapIp(freegs.jtor.ConstrainBetapIp):
             Boole mask, it is True inside the limiter. Same size as full domain grid: (eq.nx, eq.ny)
         """
         super().__init__(*args, **kwargs)
-        self.mask_inside_limiter = build_mask_inside_limiter(eq, limiter)
-        self.limiter_mask_out = plasma_grids.make_layer_mask(self.mask_inside_limiter, layer_size=1)
-        self.limiter_mask_in = plasma_grids.make_layer_mask(np.invert(self.mask_inside_limiter), layer_size=1)
+
+        self.limiter_handler = limiter_func.Limiter_handler(eq, limiter)
+        self.limiter_mask_out = plasma_grids.make_layer_mask(self.limiter_handler.mask_inside_limiter, layer_size=1)
+        self.mask_inside_limiter = self.limiter_handler.mask_inside_limiter
+
+        
         if not hasattr(self, 'fast'):
             self.Jtor = self._Jtor
         else:
@@ -64,11 +38,11 @@ class ConstrainBetapIp(freegs.jtor.ConstrainBetapIp):
         self.opt, self.xpt = critical.find_critical(R, Z, psi)
 
         self.diverted_core_mask = self.jtor>0
-        self.psi_bndry, mask, self.limiter_flag = limiter.core_mask_limiter(psi,
-                                                                        self.xpt[0][2],
-                                                                        self.diverted_core_mask,
-                                                                        self.limiter_mask_out,
-                                                                        self.limiter_mask_in)
+        self.psi_bndry, mask, self.limiter_flag = self.limiter_handler.core_mask_limiter(psi,
+                                                                                        self.xpt[0][2],
+                                                                                        self.diverted_core_mask,
+                                                                                        self.limiter_mask_out,
+                                                                                        )
         self.jtor = super().Jtor(R, Z, psi, self.psi_bndry)
         return self.jtor
 
@@ -79,11 +53,11 @@ class ConstrainBetapIp(freegs.jtor.ConstrainBetapIp):
             # print('no xpt')
             self.psi_bndry, self.limiter_core_mask, self.flag_limiter = psi_bndry, None, False
         else: 
-            self.psi_bndry, self.limiter_core_mask, self.flag_limiter = limiter.core_mask_limiter(psi,
-                                                                                                self.psi_bndry,
-                                                                                                self.diverted_core_mask,
-                                                                                                self.limiter_mask_out,
-                                                                                                self.limiter_mask_in)
+            self.psi_bndry, self.limiter_core_mask, self.flag_limiter = self.limiter_handler.core_mask_limiter(psi,
+                                                                                                                self.psi_bndry,
+                                                                                                                self.diverted_core_mask,
+                                                                                                                self.limiter_mask_out,
+                                                                                                                )
         self.jtor = super().Jtor_part2(R, Z, psi, self.psi_bndry, self.limiter_core_mask)
         return self.jtor
     
@@ -108,9 +82,11 @@ class ConstrainPaxisIp(freegs.jtor.ConstrainPaxisIp):
             Specifies the limiter contour points
         """
         super().__init__(*args, **kwargs) 
-        self.mask_inside_limiter = build_mask_inside_limiter(eq, limiter)
-        self.limiter_mask_out = plasma_grids.make_layer_mask(self.mask_inside_limiter, layer_size=1)
-        self.limiter_mask_in = plasma_grids.make_layer_mask(np.invert(self.mask_inside_limiter), layer_size=1)
+        
+        self.limiter_handler = limiter_func.Limiter_handler(eq, limiter)
+        self.limiter_mask_out = plasma_grids.make_layer_mask(self.limiter_handler.mask_inside_limiter, layer_size=1)
+        self.mask_inside_limiter = self.limiter_handler.mask_inside_limiter
+        
         if not hasattr(self, 'fast'):
             self.Jtor = self._Jtor
         else:
@@ -122,11 +98,11 @@ class ConstrainPaxisIp(freegs.jtor.ConstrainPaxisIp):
         self.opt, self.xpt = critical.find_critical(R, Z, psi)
 
         self.diverted_core_mask = self.jtor>0
-        self.psi_bndry, mask, self.limiter_flag = limiter.core_mask_limiter(psi,
-                                                                        self.xpt[0][2],
-                                                                        self.diverted_core_mask,
-                                                                        self.limiter_mask_out,
-                                                                        self.limiter_mask_in)
+        self.psi_bndry, mask, self.limiter_flag = self.limiter_handler.core_mask_limiter(psi,
+                                                                                        self.xpt[0][2],
+                                                                                        self.diverted_core_mask,
+                                                                                        self.limiter_mask_out,
+                                                                                        )
         self.jtor = super().Jtor(R, Z, psi, self.psi_bndry)
         return self.jtor
 
@@ -137,10 +113,10 @@ class ConstrainPaxisIp(freegs.jtor.ConstrainPaxisIp):
             # print('no xpt')
             self.psi_bndry, self.limiter_core_mask, self.flag_limiter = psi_bndry, None, False
         else: 
-            self.psi_bndry, self.limiter_core_mask, self.flag_limiter = limiter.core_mask_limiter(psi,
-                                                                                                self.psi_bndry,
-                                                                                                self.diverted_core_mask,
-                                                                                                self.limiter_mask_out,
-                                                                                                self.limiter_mask_in)
+            self.psi_bndry, self.limiter_core_mask, self.flag_limiter = self.limiter_handler.core_mask_limiter(psi,
+                                                                                                                self.psi_bndry,
+                                                                                                                self.diverted_core_mask,
+                                                                                                                self.limiter_mask_out,
+                                                                                                                )
         self.jtor = super().Jtor_part2(R, Z, psi, self.psi_bndry, self.limiter_core_mask)
         return self.jtor

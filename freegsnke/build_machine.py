60fd0237ebb02219bdf033dee84da3010039e12e
nicamo <n.amorisco@gmail.com>
2024-01-30 12:56:02 +0000

    typo corrected

--- freegsnke/build_machine.py
+++ freegsnke/build_machine.py
@@ -55,11 +55,7 @@ def tokamak():
                             active_coils['Solenoid']['Z']),
                             float(active_coils['Solenoid']['polarity']) * \
                             float(active_coils['Solenoid']['multiplier']),
-<<<<<<< HEAD
-=======
-
->>>>>>> 4f9a4a6 (restistance correction, limiter off initially, mastu coils)
-                    ),
+                            ),
                 ]
             ),
         ),

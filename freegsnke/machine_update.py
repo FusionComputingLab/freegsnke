88ad919ea97a9f77f6b55b0bf6517274b60be05b
George Holt <george.holt@stfc.ac.uk>
2024-05-17 15:46:18 +0000

    Updating references to FreeGSFast in machine_update

--- freegsnke/machine_update.py
+++ freegsnke/machine_update.py
@@ -1,21 +1,21 @@
-import freegs
+import freegsfast
 
 
-class Machine(freegs.machine.Machine):
-    """Same as freegs.machine.Machine.
-    It can have an additional freegs.machine.Wall object which specifies the limiter's properties.
+class Machine(freegsfast.machine.Machine):
+    """Same as freegsfast.machine.Machine.
+    It can have an additional freegsfast.machine.Wall object which specifies the limiter's properties.
     """
 
     def __init__(self, coils, wall=None, limiter=None):
-        """Instantiates the Machine, same as freegs.machine.Machine.
+        """Instantiates the Machine, same as freegsfast.machine.Machine.
 
         Parameters
         ----------
-        coils : freeGS coils[(label, Coil|Circuit|Solenoid]
+        coils : FreeGSFast coils[(label, Coil|Circuit|Solenoid]
             List of coils
-        wall : freeGS machine.Wall object
+        wall : FreeGSFast machine.Wall object
             It is only used to display the wall in plots.
-        limiter : freeGS machine.Wall object
+        limiter : FreeGSFast machine.Wall object
             This is the limiter. Used to define limiter plasma configurations.
         """
         super().__init__(coils, wall)

42ae5b2fcaa8ed6ade4ad21600d71e07e0a7b055
George Holt <george.holt@stfc.ac.uk>
2025-01-12 18:56:55 +0000

    Copy data files across for docs build

--- docs/build_documentation.sh
+++ docs/build_documentation.sh
@@ -4,8 +4,8 @@ make clean
 
 mkdir -p notebooks
 # cp "../examples/example0 - build_tokamak_machine.ipynb" notebooks
-cp "../examples/example1 - static_inverse_solve_MASTU.ipynb" notebooks
-# cp "../examples/example2 - static_forward_solve_MASTU.ipynb" notebooks
+# cp "../examples/example1 - static_inverse_solve_MASTU.ipynb" notebooks
+cp "../examples/example2 - static_forward_solve_MASTU.ipynb" notebooks
 # cp "../examples/example3 - extracting_equilibrium_quantites.ipynb" notebooks
 # cp "../examples/example4 - using_magnetic_probes.ipynb" notebooks
 # cp "../examples/example5 - evolutive_forward_solve.ipynb" notebooks
@@ -13,6 +13,10 @@ cp "../examples/example1 - static_inverse_solve_MASTU.ipynb" notebooks
 # cp "../examples/example6b - static_forward_MASTU_solves_from_magnetics_and_MSE_data.ipynb" notebooks
 # cp "../examples/example7 - static_inverse_solve_SPARC.ipynb" notebooks
 # cp "../examples/example8 - static_inverse_solve_ITER.ipynb" notebooks
+cp ../examples/limiter_currents.json notebooks
+cp ../examples/simple_diverted_currents_PaxisIp.pk notebooks
+cp ../examples/simple_limited_currents_PaxisIp.pk notebooks
+
 
 mkdir -p _images
 cp ../_images/* _images

f960fcb22840cde506a31b50d58fbe3c1d95231b
George Holt <george.holt@stfc.ac.uk>
2025-01-12 20:42:21 +0000

    Add example notebooks and data file copying to docs build script

--- docs/build_documentation.sh
+++ docs/build_documentation.sh
@@ -3,16 +3,14 @@ set -e
 make clean
 
 mkdir -p notebooks
-# cp "../examples/example0 - build_tokamak_machine.ipynb" notebooks
-# cp "../examples/example1 - static_inverse_solve_MASTU.ipynb" notebooks
+cp "../examples/example0 - build_tokamak_machine.ipynb" notebooks
+cp "../examples/example1 - static_inverse_solve_MASTU.ipynb" notebooks
 cp "../examples/example2 - static_forward_solve_MASTU.ipynb" notebooks
-# cp "../examples/example3 - extracting_equilibrium_quantites.ipynb" notebooks
-# cp "../examples/example4 - using_magnetic_probes.ipynb" notebooks
-# cp "../examples/example5 - evolutive_forward_solve.ipynb" notebooks
-# cp "../examples/example6a - static_forward_MASTU_solves_from_magnetics_only_data.ipynb" notebooks
-# cp "../examples/example6b - static_forward_MASTU_solves_from_magnetics_and_MSE_data.ipynb" notebooks
-# cp "../examples/example7 - static_inverse_solve_SPARC.ipynb" notebooks
-# cp "../examples/example8 - static_inverse_solve_ITER.ipynb" notebooks
+cp "../examples/example3 - extracting_equilibrium_quantites.ipynb" notebooks
+cp "../examples/example4 - using_magnetic_probes.ipynb" notebooks
+cp "../examples/example5 - evolutive_forward_solve.ipynb" notebooks
+cp "../examples/example7 - static_inverse_solve_SPARC.ipynb" notebooks
+cp "../examples/example8 - static_inverse_solve_ITER.ipynb" notebooks
 cp ../examples/limiter_currents.json notebooks
 cp ../examples/simple_diverted_currents_PaxisIp.pk notebooks
 cp ../examples/simple_limited_currents_PaxisIp.pk notebooks
@@ -23,6 +21,8 @@ cp ../_images/* _images
 
 mkdir -p machine_configs
 cp -r ../machine_configs/MAST-U machine_configs
+cp -r ../machine_configs/SPARC machine_configs
+cp -r ../machine_configs/ITER machine_configs
 
 sphinx-apidoc -e -f --no-toc -o api/ ../freegsnke/
 

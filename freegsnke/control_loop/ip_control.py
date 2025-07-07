45bcca9dbbb994f8d09e11bd10ce38f786a3852f
Pedro Cavestany <petercapstayn@gmail.com>
2025-07-07 16:55:16 +0100

    Clean and document ip_control module

--- freegsnke/control_loop/ip_control.py
+++ freegsnke/control_loop/ip_control.py
@@ -4,7 +4,6 @@ Module to control plasma current Ip during a tokamak shot.
 """
 
 import numpy as np
-from copy import deepcopy
 from .target_scheduler import TargetScheduler
 
 
@@ -44,10 +43,8 @@ class ControlSolenoid:
         waveform_dict,
         schedule_dict,
         sol_vc_dict,
-        pi_state,
         integral_term_0=0,
         solenoid_name=None,
-        noise_model=None,
     ):
         """
         Initialises the ControlSolenoid class.
@@ -62,19 +59,9 @@ class ControlSolenoid:
             waveform_dict, schedule_dict, sol_vc_dict, solenoid_name
         )
 
-        # print(f"  The virtual circuit vector: {self.scheduler.vc_dict}")
-
         # The accumulated error in the plasma category. Defaults to 0 if not
         # given.
         self.internal = integral_term_0
-        # self.lc_internal = integral_term_0
-        self.prev_ip_state = integral_term_0
-
-        # testing
-        self.pi_state = deepcopy(pi_state)
-
-        # self.noise_mean = noise_model['mean']
-        # self.noise_std = noise_model['std']
 
     def calculate_solenoid_delta(self,
                                  Kp,
@@ -86,131 +73,100 @@ class ControlSolenoid:
                                  Ip_obs_t,
                                  Vloop_ff,
                                  sol_vc,
-                                 pi_state,
-                                 delta_Ip_gt,
-                                 prev_delta_Ip_gt,
-                                 M_sp=1.5801*1e-5 * 1e3    # Henrys
+                                 M_sp=1.5801*1e-5 * 1e3,
+                                 dt_pi=0.0001
                                  ):
         """
         Calculates the vector of current trajectories ΔI/Δt, as prescribed
-        in the plasma category (and circuits category, supposedly) of the
-        MAST-U PCS. The equations followed are:
+        in the plasma category of the MAST-U PCS. The equations followed are:
 
         Ip_error = (Ip_req - Ip_obs)
-        integral = Ip_error * dt
-        Vloop_fb = Kp * Ip_error + Ki * (integral + 0.5 * Ip_error * dt)
-        Vloop = Vloop_fb * blend + Vloop_ff * (1 - blend)/M_sp
-        ΔIsol/Δt = -Vloop
+        integral = internal_state + 0.5 * Ip_error * dt
+        internal_state = internal_state + Ip_error * dt
+        ΔIsol_fb/Δt = Kp * Ip_error + Ki * integral
+        ΔIsol/Δt = ΔIsol_fb/Δt * blend - Vloop_ff * (1 - blend)/M_sp
         ΔI/Δt = ΔIsol/Δt * vc_vector
+
+        It should be noted that the PI controller works at a frequency twice as
+        high as the data recording system. This is why the PI controller goes
+        through two cycles in this method.
 
         Parameters
         ----------
-        - inductances_pl : dict
-            A dictionary with all the required inductacnes_pl.
         - Kp : float
-            A proportional term used in the Vloop_fb computation.
+            Proportional term used in the Vloop_fb computation.
         - Ki : float
-            An integral term used in the Vloop_fb computation.
+            Integral term used in the Vloop_fb computation.
+        - dt : float
+            Interval of time between two timestamps of the recorded data.
         - blend : float
-            A value between 0 and 1 that acts as a weight in the Vloops sum.
+            Value between 0 and 1 that acts as a weight in the Vloops sum.
         - Ip_req : float
-            The plasma current requested.
-        - Ip_obs : float
+            The plasma current requested at this PI controller cycle.
+        - Ip_obs_tprev : float
+            The plasma current observed at the previous PI controller.
+        - Ip_obs_t : float
             The plasma current observed.
         - Vloop_ff : float
             The feedforward Vloop.
+        - sol_vc : 1D numpy array
+            The solenoid Virtual Circuit. The element corresponding to the
+            solenoid of this array should always be 1.
+        - M_sp : float
+            Mutual inductance betwen the solenoid and the plasma. The default
+            value is 1.5801*1e-2 Henrys.
+        - dt_pi : float
+            Interval of time between two cycles of the PI controller. The
+            default value is 0.0001
 
         Returns
         -------
-        - dIsoldt : float
-            dIsoldt stands for ΔIsol/Δt.
+        - dI_dt : 1D numpy array
+            Array of delta currents requests that will be part of the input of
+            Circuits category.
 
         """
-        # noise = np.random.normal(loc=self.noise_mean,
-        #                          scale=self.noise_std,
-        #                          size=1)[0]
 
-        dt_pi = 0.0001
+        # PI controller cycles. The integral sum is done following the
+        # trapezoidal rule.
+        # First cycle
+
         # Compute the required change for Ip
-        previous_int = self.internal
         delta_Ip_tprev = Ip_req - Ip_obs_tprev
-        # print(f"    The delta plasma current: {delta_Ip_gt}")
-
-        # Trapezoidal rule
-        integral_tprev = self.internal + (delta_Ip_tprev * dt_pi) / 2
+        # integral_tprev = self.internal + (delta_Ip_tprev * dt_pi) / 2
         self.internal += delta_Ip_tprev * dt_pi
         # dIsol_fb_tprev = Kp * delta_Ip_tprev + Ki * integral_tprev
 
+        # Second cycle
         delta_Ip_t = Ip_req - Ip_obs_t
         integral_t = self.internal + (delta_Ip_t * dt_pi) / 2
-        # integral_t = self.internal + (delta_Ip_gt * dt) / 2
-        # self.internal = pi_state
         self.internal += delta_Ip_t * dt_pi
         dIsol_fb_t = Kp * delta_Ip_t + Ki * integral_t
-        # dIsol_fb_t2 = Kp * delta_Ip_t2 + Ki * integral_t2
-
-        # print("{:<14.10f}".format(delta_Ip_gt),
-        #       "{:<14.10f}".format(self.internal), end=' ')
-        #       "{:<14.10f}".format(integral), end=' ')
-        #       "{:<18.10f}".format(- dIsol_fb * M_sp), end=' ')
-        # This suggests that Vloop_fb is multiplied by the inductance, as an
-        # output (so it's actually a current), and that it's given in KA.
-        # print(f"    The feedback Isol for loop voltage: {dIsol_fb}")
 
         # Compute the loop voltage as a weighted sum
-        # dIsoldt = blend * Vloop_fb - (1 - blend) * Vloop_ff * (1 / M_sp)
-
-        if blend == 0:
-            dIsoldt = - Vloop_ff * (1 / M_sp)
-        elif blend == 1:
-            dIsoldt = dIsol_fb_t
-        else:
-            raise ValueError(f"Blend should either 1 or 0, not {blend}.")
-
-        # Compute the rate of change of the solenoid current
-        # print(f"    The trajectory for the solenoid current: {dIsoldt}")
+        dIsoldt = blend * dIsol_fb_t - (1 - blend) * Vloop_ff * (1 / M_sp)
 
         # Apply dIsoldt to virtual circuit vector to get the current
-        # trajectories of the active coils.
+        # trajectories requests for the active coils.
         dI_dt = dIsoldt * sol_vc
-        # print(f"    The trajectory for the solenoid current, after VC: {dI_dt[0]}")
 
         # testing
         results = {
                    "delta_Ip_tprev": delta_Ip_tprev,
                    "delta_Ip_t": delta_Ip_t,
                    "blends": blend,
-                   "delta_internal": (self.internal - previous_int),
                    "internal": self.internal,
                    "Vloop_fb": dIsol_fb_t * (-M_sp),
                    "dIsoldt": dIsoldt,
                    "dI_dt": dI_dt[0],
-                   "delta_pi_state": pi_state - self.prev_ip_state,
-                   "pi_state": pi_state,
                    }
-        self.prev_ip_state = pi_state
 
         # return dI_dt
         return results
 
-    def get_pistate(self, ts):
-        """ Auxiliar method written for testing purposes. To be deleted when
-        the code is validated
-        """
-
-        # idx = np.where(np.isclose(self.pi_state["times"], ts, atol=1e-10))[0]
-        # idx = np.where(self.pi_state["times"] == ts)[0][0]
-        idx = np.asarray(self.pi_state["times"] == ts).nonzero()[0][0]
-
-        # print("pi_state at this ts:", self.pi_state["vals"][idx])
-
-        return self.pi_state["vals"][idx]
-
     def ip_control(self,
                    ts,
                    prev_ts,
-                   delta_Ip_gt,
-                   prev_delta_Ip_gt,
                    Ip_obs_tprev=None,
                    Ip_obs_t=None,
                    eq=None):
@@ -222,13 +178,15 @@ class ControlSolenoid:
 
         Parameters
         ----------
-        - time_stamp : float (4 decimal places)
+        - ts : float (4 decimal places)
             Timestamp for which this pipeline should provide a control voltage.
-        - Rp : float
-            The plasma resistivity.
-        - inductacnes_pl : dict
-            A dictionary with all the required inductacnes_pl.
-        - Ip_obs : float
+        - prev_ts : float (4 decimal places)
+             Timestamp of the previous recorded data
+        - Ip_obs_tprev : float
+            The observed plasma current at `prev_ts` timestamp. Defaults to
+            None, in which case the observed plasma current is taken from the
+            equilibrium/control params dictionary.
+        - Ip_obs_t : float
             The observed plasma current. Defaults to None, in which case the
             observed plasma current is taken from the equilibrium/control
             params dictionary.
@@ -240,13 +198,19 @@ class ControlSolenoid:
         numpy 1D array
            Trajectories for the active coil currents due to the control on the
            solenoid coil.
+
         """
+
         if Ip_obs_t is None:
-            Ip_obs_t = self.scheduler.get_observed_current(ts, "Ip_obs", eq)
-            print(f"  Ip from equilibrium: {Ip_obs_t}")
-        else:
-            pass
-            # print(f"User defined Ip_obs: {Ip_obs_t}")
+            Ip_obs_t = self.scheduler.get_observed_current(ts,
+                                                           "Ip_obs",
+                                                           eq)
+            print(f"  Ip from equilibrium at {ts}: {Ip_obs_t}")
+        if Ip_obs_tprev is None:
+            Ip_obs_tprev = self.scheduler.get_observed_current(prev_ts,
+                                                               "Ip_obs",
+                                                               eq)
+            print(f"  Ip from equilibrium at {prev_ts}: {Ip_obs_tprev}")
 
         # Implement the plasma category. First, the relevant entities should be
         # retrieved from the scheduler
@@ -254,32 +218,25 @@ class ControlSolenoid:
             param_type="Ip", param="plasma", time_stamp=ts
         )
 
-        # TODO check if this is monitored this way
         if not Ip_req:
             print(f"  The plasma current is not controlled at t: {ts}")
             # return array of zeros if not controlled
             dI_dt = np.zeros_like(self.scheduler.vc_dict)
             return dI_dt
-        # print(f"  The requested Ip: {Ip_req}")
 
         Vloop_req = self.scheduler.get_waveform_value(
             param_type="ff", param="plasma", time_stamp=ts
         )
-        # print(f"  The requested FF_Vloop: {Vloop_req}")
 
         gain_p, _ = self.scheduler.get_gains(
                 ["plasma"], time_stamp=ts, K_type="Kprop")
         gain_int, _ = self.scheduler.get_gains(
                 ["plasma"], time_stamp=ts, K_type="Kint")
-        # print(f"  The plasma gains: Kp={gain_p}, Kint={gain_int}")
 
         blend = self.scheduler.get_waveform_value(
             param_type="blends", param="plasma", time_stamp=ts
         )
-        # print(f"  The blend value: {blend}")
-        # print("dt: ", (ts - prev_ts))
 
-        # print("{:<18.5f}".format(ts), end=' ')
         dI_dt = self.calculate_solenoid_delta(
             Kp=gain_p[0],
             Ki=gain_int[0],
@@ -290,9 +247,6 @@ class ControlSolenoid:
             Ip_obs_t=Ip_obs_t,
             Vloop_ff=Vloop_req,
             sol_vc=self.scheduler.retrieve_vc(ts),
-            pi_state=self.get_pistate(ts),
-            delta_Ip_gt=delta_Ip_gt,
-            prev_delta_Ip_gt=prev_delta_Ip_gt
         )
 
         return dI_dt

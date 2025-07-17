6436b1542e2577a81f5ec02c28f57b582a4578d5
kpentland <kpentlan@N0617-MAC-6.local>
2025-07-17 16:31:22 +0100

    minor fixes in pf_category

--- freegsnke/control_loop/pf_category.py
+++ freegsnke/control_loop/pf_category.py
@@ -1,93 +1,91 @@
-"""Small module to implement the PF category of MAST-U's PCS computer """
+"""
+Small module to implement the PF category of MAST-U control loops
+"""
 
 import numpy as np
 
-
-def calculate_voltage_pf(
+def pf_voltage_demands(
     R,
-    M_ff,
-    M_fb,
+    M_FF,
+    M_FB,
     coil_gains,
-    approved_dI_dt,
+    approved_dIdt,
     approved_I,
     measured_I,
-    # These clips follow the same coil order as the gains dictionary
-    clips=np.array([1820, 800, 900, 800, 800, 800, 800, 800, 800, 800, 800,
-                    350])
+    voltage_clips,
+    slew_rates,
+    prev_voltages,
+    dt,
 ):
     """
     Calculate the output voltage to apply on the coils, as prescribed in
-    the PF category of the PCS. The equations followed are:
-
-    V_ind = (coil_gains * (approved_I - measured_I) + approved_dI_dt) * M_fb
-    V_res = measured_I * R
-    V = V_ind + V_res
-
+    the PF category of the PCS. 
+    
     Parameters
     ----------
-    - R : numpy 1D array
-        The resistivity vector for the active coils.
-    - inductance_ff : numpy 2D array
+    R : numpy 1D array
+        The vector of resistances for the active coils.
+    M_FF : numpy 2D array
         The feedforward inductance matrix of the active coils.
-    - inductance_fb : numpy 2D array
+    M_FB : numpy 2D array
         The feedback inductance matrix of the active coils.
-    - coil_gains : numpy 2D array
-        A diagonal matrix with the coil_gains for each coil.
-    - approved_dI_dt: numpy 1D array
-        Approved change of rate of the coil currents by the system category of
-        the PCS.
-    - approved_I : numpy 1D array
-        Approved coil current by the system category of the PCS.
-    - measured_I : numpy 1D array
-        Actual measured coil currents.
+    coil_gains : numpy 1D array
+        The vector of "coil gains" for the active coils (these are timescales in seconds).
+    approved_dIdt: numpy 1D array
+        Approved rate of change of the coil currents in the active coils (provided by the
+        "system" category of the control loops). Input in A. 
+    approved_I : numpy 1D array
+        Approved coil currents in the active coils (provided by the "system" category of 
+        the control loops). Input in A. 
+    measured_I : numpy 1D array
+        Measured coil currents (from experiment or simulation) in the active coils. Input in A. 
+    voltage_clips : numpy 1D array
+        Final voltage requests are clipped if the they are outside +/- these values
+        for each of the active coils. Input in Volts. 
+    slew_rates : numpy 1D array
+        Final voltage requests are clipped again if their derivatives (wrt previous timestep) are outside 
+        +/- these values for each of the active coils. Input in Volts/s. 
+    prev_voltages : numpy 1D array
+        Voltage demands from the previous time step in the active coils. Input in Volts.  
+    dt : float
+        Time step between current and previous voltage demands. Input in seconds.   
 
     Returns
     -------
     numpy 1D array
-        Output control voltage of PCS to apply on the coils.
-
+        Voltage to apply to each of the active coils. Output in Volts. 
     """
 
-    # Compute the feedback voltage
-    # print("R shape,", np.shape(R), R)
-    # print("inductances shape,", np.shape(inductances), inductances)
-    # print("coil_gains shape,", np.shape(coil_gains), coil_gains)
-    print("approved I shape and values:,", np.shape(approved_I), approved_I)
-    print("approved dI_dt shape and values:,",
-          np.shape(approved_dI_dt), approved_dI_dt)
-    print("measured I shape and values:,", np.shape(measured_I), measured_I)
+    # resistive voltages
+    v_res = measured_I * R
+    # print(f"    The resistive voltage: \n {v_res}")
 
+    # FF voltages
+    v_FF = M_FF @ approved_dIdt
+    # print(f"    The PF feedforward voltage: \n {v_FF}")
+
+    # FB voltages
     delta_I = approved_I - measured_I
-    corrected_I = coil_gains @ delta_I
-    print("PF proportional current rate: \n", corrected_I)
+    # v_FB = M_FB @ (delta_I * coil_gains)
+    v_FB = M_FB @ (delta_I / coil_gains)
+    # print(f"    The PF feedback voltage: \n {v_FB}")
 
-    v_ff = M_ff @ approved_dI_dt
-    v_fb = M_fb @ corrected_I
-    print(f"    The PF feedforward voltage: \n {v_ff}")
-    print(f"    The PF feedback voltage: \n {v_fb}")
+    # initial voltage demands (pre-clipping)
+    v_init = v_res + v_FF + v_FB
 
-    # Compute the resistive voltage
-    v_res = measured_I * R 
-    print(f"    The resistive voltage: \n {v_res}")
+    # final voltage demands (clipped)
+    v_init_clipped_pos = np.minimum(v_init, voltage_clips)
+    v_clipped = np.maximum(v_init_clipped_pos, - voltage_clips)
+    print(f"---")
+    print(f"Absolute clipping? --> {not np.allclose(v_init, v_clipped)})")
 
-    # Combine all the voltages into the output voltage
-    v_out = (v_ff + v_fb + v_res) * 1000
-    print(f"    The full voltage: \n {v_out}")
+    # finally we apply the "slew rates", additive clipping of voltage rate of change
+    delta_voltages = v_clipped - prev_voltages
+    max_delta = slew_rates*dt
+    delta_clipped = np.clip(delta_voltages, -max_delta, max_delta)
+    v_final = prev_voltages + delta_clipped
+    print(f"Derivative clipping? --> {not np.allclose(delta_clipped, delta_voltages)})")
 
-    # Apply the clips
-    v_out_clipped_pos = np.minimum(v_out, clips)
-    v_out_clipped = np.maximum(v_out_clipped_pos, - clips)
+    print(f"    Voltages = {v_final}")
 
-    if not np.allclose(v_out, v_out_clipped):
-        print(f"    The full voltage clipped: \n {v_out_clipped}")
-
-    results = {"delta_i": delta_I,
-               "v_ff": v_ff,
-               "v_fb": v_fb,
-               "v_res": v_res,
-               "v_out": v_out,
-               "v_out_cp": v_out_clipped
-               }
-
-    # return v_out
-    return results
+    return v_final, delta_I

83ac1a1724d9accabc76a51ce74cc892d6f4ec2e
kpentland <kpentlan@N0617-MAC-6.ccfepc.ccfe.ac.uk>
2026-05-06 13:14:36 +0100

    removing legacy storage

--- freegsnke/control_loop/virtual_circuits_category.py
+++ freegsnke/control_loop/virtual_circuits_category.py
@@ -279,8 +279,8 @@ class VirtualCircuitsController:
                 self.latest_vc = VC_shape_emu
 
                 # store sensitivity matrix (Jacobian)
-                self.emulated_jacobian_list.append(self.vc_generator.jacobian_matrix)
-                self.emulated_vc_list.append(self.vc_generator.vc_matrix)
+                # self.emulated_jacobian_list.append(self.vc_generator.jacobian_matrix)
+                # self.emulated_vc_list.append(self.vc_generator.vc_matrix)
                 self.emulated_vc_times.append(t)
 
             else:

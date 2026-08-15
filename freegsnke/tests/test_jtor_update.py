1e6bf9770b5ed461bff250d3611916675277b210
kpentland <kamranpentland@hotmail.co.uk>
2026-08-15 12:01:09 +0100

    black formatting

--- freegsnke/tests/test_jtor_update.py
+++ freegsnke/tests/test_jtor_update.py
@@ -100,6 +100,7 @@ def test_profiles_BetapIp(create_machine):
         and hasattr(profiles, "jtor")
     ), "The profiles object does not have the xpt, opt and jtor attributes"
 
+
 def test_profile_geometry_is_shared_and_fallback_indices_are_lazy(create_machine):
     eq, _ = create_machine
     profiles = jtor.ConstrainPaxisIp(eq, 8.1e3, 6.2e5, 0.5)
@@ -124,8 +125,8 @@ def test_profile_geometry_is_shared_and_fallback_indices_are_lazy(create_machine
     expected_indices = np.indices((eq.nx, eq.ny)).reshape(2, -1).T
     assert np.array_equal(handler.idx_grid_points, expected_indices)
     assert handler.idx_grid_points is handler._idx_grid_points
-    
-    
+
+
 def test_contour_fallback_coordinates_and_current_sign(create_machine):
     """The contour fallback is invariant under simultaneous psi and Ip reversal."""
     eq, _ = create_machine
